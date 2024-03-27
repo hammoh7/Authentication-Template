1863162e461385217135a818f2de2c8a8d89ad53
hammoh7 <soahammohaadkar@gmail.com>
2024-03-28 00:08:12 +0530

    Library

---
+++ lib/route.ts
@@ -0,0 +1,13 @@
+export const publicRoutes = [
+    "/",
+];
+
+export const authRoutes = [
+    "/login",
+    "/register",
+    "/error",
+];
+
+export const apiAuthPrefix = "/api/auth";
+
+export const DEFAULT_LOGIN_REDIRECT = "/main";
